0036f7320835a629726d654e9883b002c230310b
Gaurav Chaubey <luciferuiop906@gmail.com>
2020-03-24 20:47:12 +0530

    Update project_code.c

--- project_code.c
+++ project_code.c
@@ -1,125 +1,69 @@
-// C++ program for implementation of RR scheduling 
-#include<iostream>
-using namespace std; 
-  
-// Function to find the waiting time for all 
-// processes 
-void findWaitingTime(int processes[], int n, 
-             int bt[], int wt[], int quantum) 
-{ 
-    // Make a copy of burst times bt[] to store remaining 
-    // burst times. 
-    int rem_bt[n]; 
-    for (int i = 0 ; i < n ; i++) 
-        rem_bt[i] =  bt[i]; 
-  
-    int t = 0; // Current time 
-  
-    // Keep traversing processes in round robin manner 
-    // until all of them are not done. 
-    while (1) 
-    { 
-        bool done = true; 
-  
-        // Traverse all processes one by one repeatedly 
-        for (int i = 0 ; i < n; i++) 
-        { 
-            // If burst time of a process is greater than 0 
-            // then only need to process further 
-            if (rem_bt[i] > 0) 
-            { 
-                done = false; // There is a pending process 
-  
-                if (rem_bt[i] > quantum) 
-                { 
-                    // Increase the value of t i.e. shows 
-                    // how much time a process has been processed 
-                    t += quantum; 
-  
-                    // Decrease the burst_time of current process 
-                    // by quantum 
-                    rem_bt[i] -= quantum; 
-                } 
-  
-                // If burst time is smaller than or equal to 
-                // quantum. Last cycle for this process 
-                else
-                { 
-                    // Increase the value of t i.e. shows 
-                    // how much time a process has been processed 
-                    t = t + rem_bt[i]; 
-  
-                    // Waiting time is current time minus time 
-                    // used by this process 
-                    wt[i] = t - bt[i]; 
-  
-                    // As the process gets fully executed 
-                    // make its remaining burst time = 0 
-                    rem_bt[i] = 0; 
-                } 
-            } 
-        } 
-  
-        // If all processes are done 
-        if (done == true) 
-          break; 
-    } 
-} 
-  
-// Function to calculate turn around time 
-void findTurnAroundTime(int processes[], int n, 
-                        int bt[], int wt[], int tat[]) 
-{ 
-    // calculating turnaround time by adding 
-    // bt[i] + wt[i] 
-    for (int i = 0; i < n ; i++) 
-        tat[i] = bt[i] + wt[i]; 
-} 
-  
-// Function to calculate average time 
-void findavgTime(int processes[], int n, int bt[], 
-                                     int quantum) 
-{ 
-    int wt[n], tat[n], total_wt = 0, total_tat = 0; 
-  
-    // Function to find waiting time of all processes 
-    findWaitingTime(processes, n, bt, wt, quantum); 
-  
-    // Function to find turn around time for all processes 
-    findTurnAroundTime(processes, n, bt, wt, tat); 
-  
-    // Display processes along with all details 
-    cout << "Processes "<< " Burst time "
-         << " Waiting time " << " Turn around time\n"; 
-  
-    // Calculate total waiting time and total turn 
-    // around time 
-    for (int i=0; i<n; i++) 
-    { 
-        total_wt = total_wt + wt[i]; 
-        total_tat = total_tat + tat[i]; 
-        cout << " " << i+1 << "\t\t" << bt[i] <<"\t "
-             << wt[i] <<"\t\t " << tat[i] <<endl; 
-    } 
-  
-    cout << "Average waiting time = "
-         << (float)total_wt / (float)n; 
-    cout << "\nAverage turn around time = "
-         << (float)total_tat / (float)n; 
-} 
-  
-// Driver code 
-int main() 
-{ 
-    // process id's 
-    int processes[] = { 1, 2, 3}; 
-    int n = sizeof processes / sizeof processes[0]; 
-  
-    // Burst time of all processes 
-    int burst_time[] = {10, 5, 8}; 
-  
-    // Time quantum 
-    int quantum = 2; 
-    findavgTime(processes, n, burst_time, quantum); 
-    return 0; 
-} 
+#include<stdio.h>
+ 
+int main()
+{
+      int i, limit, total = 0, x, counter = 0, time_quantum;
+      int wait_time = 0, turnaround_time = 0, arrival_time[10], burst_time[10], temp[10];
+      float average_wait_time, average_turnaround_time;
+      printf("\nEnter Total Number of Processes: ");
+      scanf("%d", &limit);
+      x = limit;
+      for(i = 0; i < limit; i++)
+      {
+            printf("\nEnter Details of Process[%d]\n", i + 1);
+ 
+            printf("Arrival Time: ");
+ 
+            scanf("%d", &arrival_time[i]);
+ 
+            printf("Burst Time: ");
+ 
+            scanf("%d", &burst_time[i]);
+ 
+            temp[i] = burst_time[i];
+      }
+ 
+      printf("\nEnter Time Quantum:\t");
+      scanf("%d", &time_quantum);
+      printf("\nProcess ID\t   Burst Time\t   Turnaround Time\t   Waiting Time\n");
+      for(total = 0, i = 0; x != 0;)
+      {
+            if(temp[i] <= time_quantum && temp[i] > 0)
+            {
+                  total = total + temp[i];
+                  temp[i] = 0;
+                  counter = 1;
+            }
+            else if(temp[i] > 0)
+            {
+                  temp[i] = temp[i] - time_quantum;
+                  total = total + time_quantum;
+            }
+            if(temp[i] == 0 && counter == 1)
+            {
+                  x--;
+                  printf("\nProcess[%d]\t\t%d\t\t %d\t\t\t %d", i + 1, burst_time[i], total - arrival_time[i], total - arrival_time[i] - burst_time[i]);
+                  wait_time = wait_time + total - arrival_time[i] - burst_time[i];
+                  turnaround_time = turnaround_time + total - arrival_time[i];
+                  counter = 0;
+            }
+            if(i == limit - 1)
+            {
+                  i = 0;
+            }
+            else if(arrival_time[i + 1] <= total)
+            {
+                  i++;
+            }
+            else
+            {
+                  i = 0;
+            }
+      }
+ 
+      average_wait_time = wait_time * 1.0 / limit;
+      average_turnaround_time = turnaround_time * 1.0 / limit;
+      printf("\n\nAverage Waiting Time:\t%f", average_wait_time);
+      printf("\nAvg Turn Around Time:\t%f\n", average_turnaround_time);
+      return 0;
+}
